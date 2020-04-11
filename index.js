6793282870a62ea0b485130606e2cc0bdde7d811
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-04-11 16:00:51 +0700

    Add feature show list user  and create a user

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
 const express = require('express');
 const app = express();
-
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 const port = 3000;
 
-var users = [
-  { id: 1, name: 'Thinh' },
-  { id: 2, name: 'Quynh'},
-  { id: 3, name: 'Tan'}
-];
+// Default db.json
+db.defaults({users: []}).write();
 
+// Default epress
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+// Default pug
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// Load data from index.pug up website with link: http://localhost:3000/
 app.get('/', (req, res) => {
   res.render('index');
 })
 
+// Load data: users from db.json up website with link: http://localhost:3000/users
 app.get('/users', (req, res) => {
   res.render('users/index', {
-    users: users
+    users: db.get('users').value()
   });
 })
 
+//Search a user at website: http://localhost:3000/users
 app.get('/users/search', (req, res) => {
   var q = req.query.q;
-  var matchedUsers = users.filter((user) => {
+  var matchedUsers = db.get('users').filter((user) => {
     return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  })
+  }).value()
   res.render('users/index', {
     users: matchedUsers,
     contentSearch: q
   });
+})
+
+// Load data from users/create.pug up website with link: http://localhost:3000/users/create
+app.get('/users/create', (req, res) => {
+  res.render('users/create');
+})
+
+// Add a user at website: http://localhost:3000/users/create and back website: http://localhost:3000/users
+app.post('/users/create', (req, res) => {
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
 })
 
 app.listen(port, () => {
