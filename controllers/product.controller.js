9c10e1be0438797cee0c30b916f57cd85cf965e2
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-04-17 11:32:28 +0700

    Pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,19 @@
 const db = require('../db');
 
 module.exports.index = (req, res) => {
+  var page = parseInt(req.query.page) || 1; // n - số thứ tự trang.
+  var perPage = 8; // x - số lượng sản phẩm trong 1 trang.
+
+  var start = (page - 1) * perPage;
+  var end = page * perPage;
+
+  var numberOfPages = Math.ceil(db.get('products').value().length / 8);
+  var products = db.get('products').value().slice(start, end);
+  console.log(numberOfPages);
   res.render('products/index', {
-    products: db.get('products').value()
+    //products: db.get('products').drop(start).take(perPage).value();
+    products: products,
+    numberOfPages: numberOfPages
   });
+  //console.log(numberOfPages)
 }
